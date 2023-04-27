900e10582c390e22c3bfe729cde504b0d1eb1415
mbgoodguy <nerooren4001@gmail.com>
2023-04-27 13:48:20 +0300

    deleted handler functions, added classes

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Post, Tag
+from django.views.generic import View
 # Create your views here.
 
 
@@ -9,16 +10,18 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
