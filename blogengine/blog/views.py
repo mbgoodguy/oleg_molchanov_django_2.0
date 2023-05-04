dba95a9328bbfad3f2a9ca51107d8b9fed0ba4ae
mbgoodguy <nerooren4001@gmail.com>
2023-05-04 18:46:21 +0300

    added TagDelete and PostDelete views

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
@@ -40,15 +40,24 @@ class TagUpdate(ObjectUpdateMixin, View):
     template = 'blog/tag_update_form.html'
 
 
-class TagDelete(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
 
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        tag.delete()
-        return redirect(reverse('tags_list_url'))
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     tag.delete()
+    #     return redirect(reverse('tags_list_url'))
 
 
 def tags_list(request):
