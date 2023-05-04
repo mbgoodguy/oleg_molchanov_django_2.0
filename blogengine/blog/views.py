e17e513baeda4cab13cd4128eb0f959c18de7411
mbgoodguy <nerooren4001@gmail.com>
2023-05-04 08:19:57 +0300

    added posts_list func, added ObjectCreateMixin as 1st argument in TagCreate and PostCreate

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,51 +1,37 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.views.generic import View
+from .models import Post, Tag
+from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .forms import TagForm, PostForm
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 
-from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
 
 
-# Create your views here.
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
-
-
-def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     # post = Post.objects.get(slug__iexact=slug)
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-    # def get(self, request, slug):
-    #     # tag = Tag.objects.get(slug__iexact=slug)
-    #     tag = get_object_or_404(Tag, slug__iexact=slug)
-    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
