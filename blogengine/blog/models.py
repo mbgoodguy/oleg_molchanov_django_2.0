2678aefd1f6dbcfaa34c464b0ff9c43133dee3fc
mbgoodguy <nerooren4001@gmail.com>
2023-05-04 12:00:06 +0300

    added path to PostUpdate view

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -23,6 +23,9 @@ class Post(models.Model):
             self.slug = gen_slug(self.title)
         super().save(*args, **kwargs)
 
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
@@ -36,6 +39,9 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
