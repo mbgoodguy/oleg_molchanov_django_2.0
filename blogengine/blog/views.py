6531be36052ef6892e812c2d1cae6f105013fe9e
mbgoodguy <nerooren4001@gmail.com>
2023-05-07 17:46:30 +0300

    added imported LoginRequiredMixin in our views

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -6,16 +6,47 @@ from .forms import TagForm, PostForm
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
+    raise_exception = True
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
+
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+    raise_exception = True
+
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+    raise_exception = True
+
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
+
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    raise_exception = True
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -26,38 +57,6 @@ class PostDetail(ObjectDetailMixin, View):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-
-
-class PostUpdate(ObjectUpdateMixin, View):
-    model = Post
-    model_form = PostForm
-    template = 'blog/post_update_form.html'
-
-
-class TagUpdate(ObjectUpdateMixin, View):
-    model = Tag
-    model_form = TagForm
-    template = 'blog/tag_update_form.html'
-
-
-class PostDelete(ObjectDeleteMixin, View):
-    model = Post
-    template = 'blog/post_delete_form.html'
-    redirect_url = 'posts_list_url'
-
-
-class TagDelete(ObjectDeleteMixin, View):
-    model = Tag
-    template = 'blog/tag_delete_form.html'
-    redirect_url = 'tags_list_url'
-    # def get(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
-    #
-    # def post(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     tag.delete()
-    #     return redirect(reverse('tags_list_url'))
 
 
 def tags_list(request):
