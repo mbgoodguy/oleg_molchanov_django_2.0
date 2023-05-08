6b491a4e9f7bbff0dae0f3a371ef25b4d17e2105
mbgoodguy <nerooren4001@gmail.com>
2023-05-08 20:32:41 +0300

    added atributes for pagination, if and else, context

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 
 class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
@@ -66,4 +67,28 @@ def tags_list(request):
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_page_url': next_url,
+        'prev_page_url': prev_url,
+
+    }
+    return render(request, 'blog/index.html', context=context)
+
